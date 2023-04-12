bbc404c58ede949aa15eb33ba1e8116ea5918e5b
Zhenych-Kh <zhenych.kh@gmail.com>
2023-04-12 11:28:29 +0300

    Add project files.

---
+++ imageProcessing/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace imageProcessing
+{
+    public partial class Form1 : Form
+    {
+        private List<Bitmap> _bitMaps = new List<Bitmap>();
+        private Random _random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Select picture";
+        }
+
+        private async void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var sv = Stopwatch.StartNew();
+                menuStrip1.Enabled = trackBar1.Enabled = false;
+                pictureBox1.Image = null;
+                _bitMaps.Clear();
+                var bitMap = new Bitmap(openFileDialog1.FileName);
+                await Task.Run(() => { RunProcessing(bitMap); });
+                menuStrip1.Enabled = trackBar1.Enabled = true;
+                Text = "Load time: " + sv.Elapsed.ToString();
+            }
+        }
+
+        private void RunProcessing(Bitmap bitmap)
+        {
+            var pixels = GetPixels(bitmap);
+            var pixelInStep = (bitmap.Width * bitmap.Height) / 100;
+            var currentPixelsSet = new List<Pixel>(pixels.Count - pixelInStep);
+
+            for (int i = 0; i < trackBar1.Maximum; i++)
+            {
+                for (int j = 0; j < pixelInStep; j++)
+                {
+                    var index = _random.Next(pixels.Count);
+                    currentPixelsSet.Add(pixels[index]);
+                    pixels.RemoveAt(index);
+                }
+                var currentBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+
+                foreach (var pixel in currentPixelsSet)
+                    currentBitmap.SetPixel(pixel.Point.X, pixel.Point.Y, pixel.Color);
+                _bitMaps.Add(currentBitmap);
+
+                this.Invoke(new Action(() =>
+                {
+                    Text = $"Load: {i}%";
+                }));
+            }
+            _bitMaps.Add(bitmap);
+        }
+
+        private List<Pixel> GetPixels(Bitmap bitmap)
+        {
+            var pixels = new List<Pixel>(bitmap.Width * bitmap.Height);
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    pixels.Add(new Pixel()
+                    {
+                        Color = bitmap.GetPixel(x, y),
+                        Point = new Point() { X = x, Y = y },
+                    });
+                }
+            }
+            return pixels;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (_bitMaps == null || _bitMaps.Count == 0)
+                return;
+
+            pictureBox1.Image = _bitMaps[trackBar1.Value - 1];
+        }
+    }
+}
